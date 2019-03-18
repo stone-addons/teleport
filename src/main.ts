5ee4f44f22d3ec7bdc4351ba781f319337befcbf
Code Hz <CodeHz@outlook.com>
2019-03-18 21:25:16 +0800

    first commit

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,45 @@
-import { MySystem } from "./system";
+import { system, MySystem } from "./system";
+import { isPlayerInfo } from "./utils";
 
-const system = server.registerSystem<MySystem>(0, 0);
 system.initialize = function() {
-  server.log("Mod Loaded");
+  server.log("Teleport Addon Loaded");
+  this.registerCommand("home", {
+    description: "commands.custom.home",
+    permission: 0,
+    overloads: [
+      {
+        parameters: [],
+        handler(origin) {
+          if (!origin.entity) throw "Player required";
+          const info = this.actorInfo(origin.entity);
+          if (isPlayerInfo(info)) {
+            const [x, y, z] = info.spawnPoint;
+            if (info.dim !== 0) throw "Cannot cross-dimension teleport";
+            if (x === 0 && y === -1 && z === 0) throw "No home found!";
+            this.openModalForm(
+              origin.entity,
+              JSON.stringify({
+                type: "modal",
+                title: "Teleport Menu",
+                content: `Do you want to teleport to home(${x}, ${y}, ${z})`,
+                button1: "Yes",
+                button2: "No"
+              })
+            )
+              .then(sel => {
+                if (JSON.parse(sel) === true) {
+                  const component = this.createComponent(
+                    origin.entity,
+                    MinecraftComponent.Position
+                  );
+                  Object.assign(component, { x, y, z });
+                  this.applyComponentChanges(origin.entity, component);
+                }
+              })
+              .catch(server.log);
+          } else throw "Failed to teleport";
+        }
+      } as CommandOverload<MySystem, []>
+    ]
+  });
 };
--- src/system.ts
+++ src/system.ts
@@ -3,3 +3,5 @@ export type MySystem = SystemType<PrivSystem>;
 interface PrivSystem extends IStoneServerSystem<PrivSystem> {}
 
 checkApiLevel(1);
+
+export const system = server.registerSystem<MySystem>(0, 0);
