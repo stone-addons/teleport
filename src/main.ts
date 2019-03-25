1027a8cf40b76be5944b994def52e95f70fc511e
Code Hz <CodeHz@outlook.com>
2019-03-25 15:54:17 +0800

    add spawn support

--- src/main.ts
+++ src/main.ts
@@ -42,4 +42,44 @@ system.initialize = function() {
       } as CommandOverload<MySystem, []>
     ]
   });
+  this.registerCommand("spawn", {
+    description: "commands.custom.spawn",
+    permission: 0,
+    overloads: [
+      {
+        parameters: [],
+        handler(origin) {
+          if (!origin.entity) throw "Player required";
+          const info = this.actorInfo(origin.entity);
+          const world = this.worldInfo();
+          if (isPlayerInfo(info)) {
+            const [x, y, z] = world.spawnPoint;
+            if (info.dim !== 0) throw "Cannot cross-dimension teleport";
+            if (y === 32767) throw "Cannot detect spawnpoint altitude, you need to use /setworldpoint at first";
+            this.openModalForm(
+              origin.entity,
+              JSON.stringify({
+                type: "modal",
+                title: "Teleport Menu",
+                content: `Do you want to teleport to spawn(${x}, ${y}, ${z})`,
+                button1: "Yes",
+                button2: "No"
+              })
+            )
+              .then(sel => {
+                if (JSON.parse(sel) === true) {
+                  const component = this.createComponent(
+                    origin.entity,
+                    MinecraftComponent.Position
+                  );
+                  Object.assign(component.data, { x, y, z });
+                  this.applyComponentChanges(origin.entity, component);
+                }
+              })
+              .catch(server.log);
+          } else throw "Failed to teleport";
+        }
+      } as CommandOverload<MySystem, []>
+    ]
+  });
 };
