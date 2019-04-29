1a8e6867bc82aac5872677b5884b3d5dfb333962
Code Hz <CodeHz@outlook.com>
2019-04-29 18:11:42 +0800

    add tpa

--- src/main.ts
+++ src/main.ts
@@ -55,7 +55,8 @@ system.initialize = function() {
           if (isPlayerInfo(info)) {
             const [x, y, z] = world.spawnPoint;
             if (info.dim !== 0) throw "Cannot cross-dimension teleport";
-            if (y === 32767) throw "Cannot detect spawnpoint altitude, you need to use /setworldpoint at first";
+            if (y === 32767)
+              throw "Cannot detect spawnpoint altitude, you need to use /setworldpoint at first";
             this.openModalForm(
               origin.entity,
               JSON.stringify({
@@ -80,6 +81,62 @@ system.initialize = function() {
           } else throw "Failed to teleport";
         }
       } as CommandOverload<MySystem, []>
+    ]
+  });
+  this.registerCommand("tpa", {
+    description: "commands.custom.tpa",
+    permission: 0,
+    overloads: [
+      {
+        parameters: [
+          {
+            type: "player-selector",
+            name: "target"
+          },
+          {
+            type: "message",
+            name: "message",
+            optional: true
+          }
+        ],
+        handler(origin, [players, msg]) {
+          if (
+            !origin.entity ||
+            origin.entity.__identifier__ !== "minecraft:player"
+          )
+            throw "Player required";
+          if (players.length !== 1)
+            throw `Cannot teleport to ${players.length} target(s)`;
+          const info = this.actorInfo(origin.entity) as PlayerInfo;
+          if (!info) throw `Cannot found actor info`;
+          const target = players[0];
+          this.openModalForm(
+            target,
+            JSON.stringify({
+              type: "modal",
+              title: "Teleport Request",
+              content: `${info.name} want teleport to you: ${msg}`,
+              button1: "Yes",
+              button2: "No"
+            })
+          )
+            .then(sel => {
+              if (JSON.parse(sel) === true) {
+                const oth = this.getComponent(
+                  target,
+                  MinecraftComponent.Position
+                );
+                const component = this.createComponent(
+                  origin.entity,
+                  MinecraftComponent.Position
+                );
+                Object.assign(component.data, oth.data);
+                this.applyComponentChanges(origin.entity, component);
+              }
+            })
+            .catch(server.log);
+        }
+      } as CommandOverload<MySystem, ["player-selector", "message"]>
     ]
   });
 };
